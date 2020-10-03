571f1936be97d208bc7b47878518429b938df70e
zkerriga <daniil.63@yandex.ru>
2020-10-03 15:00:20 +0300

    Update table -> init philosophers

--- philo_one/classes/table/table_constructors.c
+++ philo_one/classes/table/table_constructors.c
@@ -18,9 +18,12 @@ static void	table_del(t_table *self)
 
 	i = 0;
 	while (i < self->quantity)
+		self->philosophers_array[i]->del(self->philosophers_array[i]);
+	i = 0;
+	while (i < self->quantity)
 		pthread_mutex_destroy(&(self->forks_array[i++]));
 	free(self);
-	D(puts("[+] The table was deleted successfully!");)
+	D(puts("[+] The table was deleted successfully!"))
 }
 
 static pthread_mutex_t	*create_forks_array(size_t quantity)
@@ -45,15 +48,49 @@ static pthread_mutex_t	*create_forks_array(size_t quantity)
 	return (forks_array);
 }
 
+static t_philosopher	**create_philo_array(t_table *self, const t_args *args)
+{
+	t_philosopher	**philo_array;
+	size_t			i;
+
+	if ((philo_array = malloc(sizeof(t_philosopher *) * args->number_of_philosophers)))
+	{
+		i = 0;
+		while (i < args->number_of_philosophers)
+		{
+			philo_array[i] = philosopher_new(args, &self->born, i + 1,  &(self->forks_array[i]), &(self->forks_array[(i + 1 == args->number_of_philosophers) ? 0 : i + 1]));
+			if (!philo_array[i])
+			{
+				while (i)
+				{
+					--i;
+					philo_array[i]->del(philo_array[i]);
+				}
+				return (NULL);
+			}
+			++i;
+		}
+	}
+	return (philo_array);
+}
+
 t_table		*table_new(const t_args *args)
 {
 	t_table	*self;
 
 	if ((self = (t_table *)malloc(sizeof(t_table))))
 	{
+		self->born = 0;
 		self->quantity = args->number_of_philosophers;
 		if (!(self->forks_array = create_forks_array(self->quantity)))
 		{
+			free(self);
+			return (NULL);
+		}
+		if (!(self->philosophers_array = create_philo_array(args, &self->born)))
+		{
+			while (self->quantity)
+				pthread_mutex_destroy(&(self->forks_array[--self->quantity]));
 			free(self);
 			return (NULL);
 		}
