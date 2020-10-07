1132fe7eb6a5a4a59126f22a876c877a6232b42d
zkerriga <daniil.63@yandex.ru>
2020-10-07 10:47:17 +0300

    Remove lifetime last version

--- philo_one/classes/philosopher/philosopher_lifetime.c
+++ philo_one/classes/philosopher/philosopher_lifetime.c
@@ -12,15 +12,6 @@
 
 #include "philosopher.h"
 
-/*static int	time_was_changed(t_philosopher *self, size_t last_eat_time,
-								size_t *eat_time_cp)
-{
-	pthread_mutex_lock(&self->eat_mutex);
-	*eat_time_cp = self->eat_time;
-	pthread_mutex_unlock(&self->eat_mutex);
-	return (*eat_time_cp > last_eat_time);
-}*/
-
 void		*philosopher_lifetime(t_philosopher *self)
 {
 	const int	delay = 500;
@@ -49,36 +40,3 @@ void		*philosopher_lifetime(t_philosopher *self)
 	}
 	return (THREAD_SUCCESS);
 }
-
-/*
-** This method is a thread that keeps track of the philosopher's lifetime.
-** As soon as the philosopher dies, the table object receives information
-** about this via the `someone_died` field, after which it begins waiting
-** for the death message to finish printing.
-*/
-
-/*
-void		*philosopher_lifetime(t_philosopher *self)
-{
-	size_t		next_sleep_time;
-	size_t		last_eat_time;
-	size_t		eat_time_cp;
-
-	while (!(*self->born))
-		;
-	set_time_usec(&last_eat_time);
-	ft_usleep(self->stats->time_to_die);
-	while (time_was_changed(self, last_eat_time, &eat_time_cp))
-	{
-		next_sleep_time = eat_time_cp - last_eat_time;
-		last_eat_time = eat_time_cp;
-		ft_usleep(next_sleep_time);
-	}
-	if (!*self->someone_died && self->eat_counter != 0)
-	{
-		*self->someone_died = 1;
-		self->say(self, SAY_DIE, 1);
-	}
-	return (THREAD_SUCCESS);
-}
-*/
